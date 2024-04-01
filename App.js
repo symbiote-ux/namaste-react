1cb7f74ee162b64e47f7c1899feed536884fca30
Saurabh Chauhan <tnikhil422@gmail.com>
2024-04-01 08:31:09 +0530

    extract script into App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  'h1',
+  {},
+  'Hello World from React Inside It'
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
