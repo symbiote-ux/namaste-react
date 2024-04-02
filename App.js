7ea8f0482192da4781c1124c319f1fe68fa8db00
Saurabh Chauhan <tnikhil422@gmail.com>
2024-04-02 07:26:21 +0530

    add npm

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'I am H1 tag'),
+    React.createElement('h2', {}, 'I am H2 tag'),
+  ])
+);
+
 const heading = React.createElement(
   'h1',
-  {},
+  { id: 'heading', xyz: 'abc' },
   'Hello World from React Inside It'
 );
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parent);
+ 
